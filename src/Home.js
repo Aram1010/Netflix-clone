f96ba946a5d00e6801c1e00e64d018d5fcc20f6d
Arman777777 <arman.manukyan4.y@gmail.com>
2022-07-30 19:31:57 +0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,8 @@ function Home() {
   useEffect(() => {
     const navigation = document.querySelector(".navigation");
     navigation.style.background = "#191932";
-    navigation.style.boxShadow = "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px"
+    navigation.style.boxShadow =
+      "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px";
   }, []);
 
   return (
